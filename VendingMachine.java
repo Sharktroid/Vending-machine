3b99e0f544fb1a78a33e5cf4e975a68e610ff42a
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2024-02-17 10:46:10 -0500

    Snacks now have associated prices

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,17 +1,16 @@
-import java.util.ArrayList; 
+import java.util.Hashtable;
 
-public class VendingMachine
-{   public VendingMachine(){
-    ArrayList<String> snacks = new ArrayList<String>();
-    snacks.add("Doritos");
-    snacks.add("Oreo");
-    snacks.add("M&M's");
-    snacks.add("Lay's");
-    snacks.add("Cheetos");
-
-}   
-    public static void main(String[] args)
-    {
+public class VendingMachine {
+    private Hashtable<String, Double> snacks;
+    public VendingMachine() {
+        snacks = new Hashtable<String, Double>();
+        snacks.put("Doritos", 2.24);
+        snacks.put("Oreo", 2.49);
+        snacks.put("M&M's", 1.99);
+        snacks.put("Lay's", 3.24);
+        snacks.put("Cheetos", 2.99);
+    }
+    public static void main(String[] args) {
 
     }
 }
