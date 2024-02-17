49589ead14c52c41dfc8f6da76fce15b878dffa3
Requiem493 <akshaj.illa@gmail.com>
2024-02-17 07:03:43 -0500

    Added Arraylist of Snacks

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList; 
+
 public class VendingMachine
-{
+{   public VendingMachine(){
+    ArrayList<String> snacks = new ArrayList<String>();
+    snacks.add("Doritos");
+    snacks.add("Oreo");
+    snacks.add("M&M's");
+    snacks.add("Lay's");
+    snacks.add("Cheetos");
+
+}   
     public static void main(String[] args)
     {
 
