08f95e4580ebd9051cc3cc5b12f9364373e17ea6
Requiem493 <akshaj.illa@gmail.com>
2024-02-17 15:31:03 -0500

    Added usage instructions

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,8 +1,15 @@
 import java.util.Hashtable;
+import java.util.Scanner;
+
 
 public class VendingMachine {
     private Hashtable<String, Double> snacks;
+    private static Scanner scan;
+    private static String snackChoice;
+
     public VendingMachine() {
+        scan =  new Scanner(System.in);
+        snackChoice = "";
         snacks = new Hashtable<String, Double>();
         snacks.put("Doritos", 2.24);
         snacks.put("Oreo", 2.49);
@@ -11,6 +18,9 @@ public class VendingMachine {
         snacks.put("Cheetos", 2.99);
     }
     public static void main(String[] args) {
-
+        System.out.println("We're happy you decided to use our Vending Machine today!");
+        System.out.println("Please select a snack to buy:");
+        snackChoice = scan.next();
+        //I have no idea what a Hashtable is. Sorry :(
     }
 }
